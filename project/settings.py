34c110ce04a13921f4bb651a35d78db9fc6b37da
Timi <tunmisebabade@gmail.com>
2024-02-03 15:32:55 +0100

    first commit

--- project/settings.py
+++ project/settings.py
@@ -89,7 +89,9 @@ DATABASES = {
 }
 
 database_url = os.environ.get("DATABASE_URL")
-DATABASES['default'] = dj_database_url.parse(database_url)
+DATABASES = {
+    'default': dj_database_url.parse(database_url)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
