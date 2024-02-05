c09db3e8afcd08677e75835cdf6aa726dd653143
Timi <tunmisebabade@gmail.com>
2024-02-05 11:46:23 +0100

    first commit

--- project/settings.py
+++ project/settings.py
@@ -18,19 +18,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
-security_key = os.environ.get("SECURITY_KEY")
-SECRET_KEY = security_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
+
+# SECURITY WARNING: keep the secret key used in production secret!
+security_key = os.environ.get("SECURITY_KEY")
+SECRET_KEY = security_key
 
 # Application definition
 
@@ -48,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -140,4 +137,4 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_ALL_ORIGINS = True
